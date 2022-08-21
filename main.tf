33488a93aefd075ad22f4f0d087f2f57fa9ddb84
Jacob Nosal <jacobnosal@gmail.com>
2022-08-21 01:46:34 -0500

    Add budget for s3.

--- main.tf
+++ main.tf
@@ -34,3 +34,31 @@ resource "aws_budgets_budget" "account_budget" {
     subscriber_email_addresses = [var.notification_emails]
   }
 }
+
+resource "aws_budgets_budget" "s3_budget" {
+  name              = "monthly_account_budget"
+  limit_amount      = var.budget_limit_amount_s3_in_gb
+  budget_type       = "USAGE"
+  limit_unit        = "GB"
+  time_period_end   = var.budget_time_end
+  time_period_start = var.budget_time_start
+  time_unit         = var.budget_time_unit
+
+  # Send a notification when actual spending reaches 80% of the limit.
+  notification {
+    comparison_operator        = "EQUAL_TO"
+    threshold                  = 80
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = [var.notification_emails]
+  }
+
+  # Send a notification when actual spending reaches threshhold.
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = [var.notification_emails]
+  }
+}
