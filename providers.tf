63d0c06db4c7eca034c6a68f918038181efc1cca
Jacob Nosal <jacobnosal@gmail.com>
2022-08-21 01:39:05 -0500

    Add remote state.

--- providers.tf
+++ providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = ">= 4.0"
     }
   }
+
+  backend "s3" {
+    bucket = "tfstate-jnosal"
+    key    = "aws-account-administration/terraform.tfstate"
+    region = "us-west-2"
+  }
 }
 
 provider "aws" {}
