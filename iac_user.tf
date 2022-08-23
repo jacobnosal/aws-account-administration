1bb6172b410303fcb8de1c1d3e448d3f4316f514
Jacob Nosal <jacobnosal@gmail.com>
2022-08-22 23:28:14 -0500

    Add user access key and outputs.

--- iac_user.tf
+++ iac_user.tf
@@ -3,6 +3,10 @@ resource "aws_iam_user" "iac_user" {
   path = "/"
 }
 
+resource "aws_iam_access_key" "iac_user_access_key" {
+  user = aws_iam_user.iac_user.name
+}
+
 resource "aws_iam_user_group_membership" "iac_user__iac_contributor" {
   user   = aws_iam_user.iac_user.name
   groups = [aws_iam_group.aws_iac_contributor.name]
