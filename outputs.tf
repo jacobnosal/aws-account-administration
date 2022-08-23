95cfec5eb6864a25286fe5c2114021d281243bea
Jacob Nosal <15789559+jacobnosal@users.noreply.github.com>
2022-08-22 23:31:23 -0500

    Add aws_iac_contributor group and iac_user user. (#4)
    - Adds `aws_iam_group.aws_iac_contributor`, `aws_iam_user.iac_user` and `aws_iam_access_key.iac_user_access_key`

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,13 @@ output "account_budget_id" {
 output "account_budget_arn" {
   value = aws_budgets_budget.account_budget.arn
 }
+
+output "iac_user_access_key_id" {
+  value     = aws_iam_access_key.iac_user_access_key.id
+  sensitive = true
+}
+
+output "iac_user_access_key_secret" {
+  value     = aws_iam_access_key.iac_user_access_key.secret
+  sensitive = true
+}
